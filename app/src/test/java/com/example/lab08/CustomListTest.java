ebaa886e530a66bed1252ff0ba64de8e41c8a1bb
Kishan200416 <ghetiakishan16@gmail.com>
2025-10-28 17:22:07 -0600

    RED: Add failing test for hasCity

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -4,4 +4,16 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 public class CustomListTest {
 
+    @Test
+    void testHasCity() {
+        CustomList list = new CustomList();
+        City city = new City("Edmonton", "Alberta");
+
+        // The 'hasCity' method doesn't exist yet, so this line will cause an error!
+        assertFalse(list.hasCity(city));
+
+        list.addCity(city);
+        assertTrue(list.hasCity(city));
+    }
+
 }
