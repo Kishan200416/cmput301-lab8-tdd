dc9d05401bb328949ada1cbdf1b9238ec9991159
Kishan200416 <ghetiakishan16@gmail.com>
2025-10-28 21:52:48 -0600

    RED: countCities has not implemented

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -9,7 +9,7 @@ public class CustomListTest {
         CustomList list = new CustomList();
         City city = new City("Edmonton", "Alberta");
 
-        // The 'hasCity' method doesn't exist yet, so this line will cause an error!
+        // HasCity Exists Now
         assertFalse(list.hasCity(city));
 
         list.addCity(city);
@@ -22,11 +22,24 @@ public class CustomListTest {
         City city = new City("Edmonton", "Alberta");
         list.addCity(city); // Add a city so we can test deleting it
 
-        // This 'deleteCity' method doesn't exist yet, so it will fail
+        // This 'deleteCity' method does exist now, so it will not fail
         list.deleteCity(city);
 
         // We expect the city to be gone
         assertFalse(list.hasCity(city));
+    }
+
+    @Test
+    void testCountCities() {
+        CustomList list = new CustomList();
+
+        // Count should be 0 for a new list
+        // This 'countCities' method doesn't exist yet, so it will fail for now atleast
+        assertEquals(0, list.countCities());
+
+        // Test 2: Count should be 1 after adding a city
+        list.addCity(new City("Edmonton", "Alberta"));
+        assertEquals(1, list.countCities());
     }
 
 }
